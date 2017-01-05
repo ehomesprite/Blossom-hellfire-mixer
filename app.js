d87d00f4f02f5f5236d5a9430769f485be48c24e
lzqyydy <lzqyydy@gmail.com>
2017-01-05 23:55:54 +0800

    minor fix and introducing socket.io
    still need to improve the usage of socket

--- app.js
+++ app.js
@@ -13,14 +13,18 @@ var blog = require('./routes/blog');
 var oti = require('./routes/oti');
 var apis_blog = require('./routes/apis_blog');
 var apis_lolitaur = require('./routes/apis_lolitaur');
+var socket_snowchat = require('./routes/socket_snowchat');
 var app = express();
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(favicon(path.join(__dirname, '../static', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json({limit: '5mb'}));
@@ -62,6 +66,8 @@ app.use('/apis', function(req, res, next) {
 app.use('/apis/blog', apis_blog);
 app.use('/apis/lolitaur', apis_lolitaur);
 
+socket_snowchat(io);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -94,4 +100,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+module.exports = {app: app, server: server};
---
+++ routes/socket_snowchat.js
@@ -0,0 +1,14 @@
+var ioResponse = function(io){
+  io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+  });
+}
+
+
+module.exports = ioResponse;
